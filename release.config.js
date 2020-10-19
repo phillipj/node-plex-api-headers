634892de12dfc1c0ece1a5abdcbad619835f5f24
Phillip Johnsen <johphi@gmail.com>
2020-10-19 19:54:46 +0200

    chore(ci): use semantic-release to cut releases

---
+++ release.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/npm",
+      {
+        tarballDir: "release",
+        usesAutomationToken: true,
+      },
+    ],
+    [
+      "@semantic-release/github",
+      {
+        assets: "release/*.tgz",
+      },
+    ],
+    "@semantic-release/git",
+  ],
+  preset: "angular",
+};
